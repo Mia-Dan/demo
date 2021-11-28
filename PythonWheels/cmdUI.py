24eb83b804f018d8dec6efce075774ce321e8db1
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2021-11-28 21:27:33 +0800

    add dragFileHere()

--- PythonWheels/cmdUI.py
+++ PythonWheels/cmdUI.py
@@ -1,14 +1,33 @@
-
 # Python在terminal运行时的输出样式，一些常用的小函数
-
 import time
 
+# ------ input ------
+
+# TODO: allow whitespace in filepath.
+def dragFileHere():
+    ''' Ask user to drag a file into Terminal.
+        * Currently, NO whitespace is allowed in filepath.
+    Returns:
+        If a file is dragged in, return its filepath(str).
+        If nothing is dragged in, return None.
+    '''
+    filepath = input("Drag your file below:\n").strip() # Dragings are likely to leave a whitespace at end
+    if filepath == b'': # NOTE: not '', but b''.
+        print("Nothing Entered.")
+        return
+    return filepath
+
+
+# ------ output ------
+
+# TODO: [BUG?] only work in sublime console, but fail in Terminal.
 def displayLineByChar(strLine, period=0.1):
-    '''display line by character 将一行文字逐字符显示
-    TODO:?BUG only work in sublime console, but fail in Terminal.
-    para: 
-    strLine[str] 要打印的一行文字
-    period[float] 每两个字符打印的时间间隔
+    ''' Display line by character 将一行文字逐字符显示
+    Parameters:  
+        strLine[str] 要打印的一行文字
+        period[float] 每两个字符打印的时间间隔
+    Returns: 
+        None
     '''
     for ch in strLine:
         print(ch, end='')
@@ -16,16 +35,23 @@ def displayLineByChar(strLine, period=0.1):
     print('') # 换行
 
 def displayLinesByChar(strLines, periodCh=0.1, periodLine=0.3):
-    '''display lines by character 将一段或多段文字逐字符显示, 文字可含换行符
-    para: 
-    strLines[str] 要打印的一段或多段文字
-    period[float] 每两个字符打印的时间间隔
+    ''' Display lines by character 将一段或多段文字逐字符显示, 文字可含换行符
+    Parameters:  
+        strLines[str] 要打印的一段或多段文字
+        period[float] 每两个字符打印的时间间隔
+    Returns: 
+        None
     '''
     strLineList = strLines.split('\n')
     for strLine in strLineList:
         displayLineByChar(strLine, periodCh)
         time.sleep(periodLine)
 
+# dragFileHere() - Test Cases
+# dragFileHere()
+# drag nothing
+# drag file without whitespaces 
+# drag file with whitespaces ❌
 
 # displayLineByChar() - Test Cases
 displayLineByChar("")
