4f578671fd27a33629afd5f678703d685ef0fcde
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2021-11-30 21:34:08 +0800

    debug

--- PythonWheels/extractMP3FromMP4.py
+++ PythonWheels/extractMP3FromMP4.py
@@ -1,13 +1,24 @@
 from pydub import AudioSegment
+from cmdUI import dragFileHere # get filename by dragging file into Terminal
 
-mp4Filename = b''
-while mp4Filename == b'':
-    mp4Filename = input("Drag MP4 below:\n").strip() # Draging are likely to leave a whitespace 
-# at the end of string, which then causes #FileNotFoundError: [Errno 2] No such file or directory
-mp3Filename = input("Output MP3 as: (by default, name as 1.mp3 on desktop)\n").encode('unicode-escape')
-isNoEnter = (mp3Filename == b"") 
-#isNoEnter = (mp3Filename == "") #AttributeError: 'bytes' object has no attribute 'seek'
-#print(mp3Filename) 
+print("Input mp4 file - ")
+mp4Filename = dragFileHere()
+print(mp4Filename) #T
+
+mp3Filename = input("Output MP3 as: (by default, same_name.mp3 in same_dir)\n")
+#isNoEnter = (mp3Filename == b"") 
+#isNoEnter = (mp3Filename == "") #AttributeError: 'bytes' object has no attribute 'seek' 
+# no, that's because "mp3Filename = input("...").encode('unicode-escape')"
+isNoEnter = (mp3Filename == "")
 if isNoEnter: 
     mp3Filename = "/Users/admin/Desktop/1.mp3"
+print(mp3Filename) #T
 AudioSegment.from_file(mp4Filename).export(mp3Filename, format='mp3')
+
+
+
+
+
+
+
+
