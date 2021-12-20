c6078aabdbd4682a40fec7a9b5a1f8bdbefd6144
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2021-12-20 09:16:45 +0800

    Update tmp_readFile.py

--- PythonProjects/10TextFormat/tmp_readFile.py
+++ PythonProjects/10TextFormat/tmp_readFile.py
@@ -15,44 +15,71 @@ BP      Beginning Page
 EP      Ending Page
 '''
 
-# TODO: format AU names
+# TBC: format AU names
 
 filepath = "savedrecs.txt"
 outputFilepath = "formatted.txt"
-targetAttr = ['PT', 'AU', 'TI', 'SO', 'VL', 'IS', 'BP', 'EP', 'PY']
+targetKey = ['PT', 'AU', 'TI', 'SO', 'VL', 'IS', 'BP', 'EP', 'PY']
 pb = {}
 key = ''
 
 def formatPb(pb):
-    ''' Format one entry -> GB/T 7714—2005 
+    ''' Format one dictionary -> GB/T 7714—2005 bibliography
     Example: Kanamori H．Shaking without quaking[J]．Science，1998，279(5359)：2063-2064．
     '''
     return f"{pb['AU']}. {pb['TI']}[{pb['PT']}]. {pb['SO']}, {pb['PY']}, {pb['VL']}({pb['IS']}): {pb['BP']}-{pb['EP']}.\n"
-                    
+  
+def formatValue(key, raw):
+    '''
+    '''
+    if key == 'AU':
+        return formatAU(raw)
+    else: 
+        return raw
+
+def appendValue(pb, key, valueField):
+    '''
+    '''
+    if key == 'AU':
+        pb[key] += ', ' + formatValue('AU', valueField)
+    else:
+        pb[key] += ' ' + formatValue(key, valueField)
+
+def formatAU(au):
+    ''' Format authors -> GB/T 7714—2005
+    Example: J. C. Smith -> Smith J C; Albert Einstein -> Einstein A; Hiroo Kanamori -> Kanamori H; Liang Fujun
+    '''
+    # au.replace('.', '') #%%%
+    # print(au)
+    # [lastNames, firstNames] = au.split(', ')
+    # firstNames = firstNames.split( )
+    # firstName = str(firstNames[-1])
+    # print(firstName)
+    # upper = []
+    # # upper = re.#%%%
+    # # upper += re.#%%%
+    # return f"#{firstName} {' '.join(upper)}#"
+
+    [lastNames, firstNames] = au.split(', ')
+    au = ' '.join([firstNames, lastNames])
+    return au
+
 with open(outputFilepath,'a') as fOut:
     with open(filepath) as f:
         for line in f:
-            if line[0:2] == '  ':
-                if key in targetAttr:
-                    pb[key] += ' ' + line[3:-1]
-            else: 
-                key = line[0:2]
-                if key in targetAttr:
-                    pb[key] = line[3:-1]
-                elif key == '\n':
-                    ref = formatPb(pb)
-                    fOut.write(ref)
+            if line == '\n':
+                ref = formatPb(pb)
+                fOut.write(ref)
+                continue
 
-        # if line[0:2] == 'TI':
-        #     flag = True
-        #     content.append(str(i)+'\t'+line[3:])
-        #     i += 1
-        # if flag == True:
-        #     if line[0:2] != 'TI':
-        #         flag = False
-        #     if line[0:2] == '  ':
-        #         content.append(line)
-# print(content)
+            keyField = line[0:2]; valueField = line[3:-1]
+            if keyField == '  ': # continue last line
+                if key in targetKey:
+                    appendValue(pb, key, valueField)
+            else: 
+                key = keyField
+                if key in targetKey:
+                    pb[key] = formatValue(key, valueField)
 
 # outputpath = '/Users/admin/Downloads/output.txt'
 # with open(outputpath, 'w') as f:
