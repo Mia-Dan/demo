b2df3495ab2df700bc8b97e13e79db39c551125e
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2021-12-22 10:14:05 +0800

    format code in a hash

--- PythonProjects/10TextFormat/tmp_readFile.py
+++ PythonProjects/10TextFormat/tmp_readFile.py
@@ -1,3 +1,4 @@
+# Bibliography formatting (GB/T 7714—2005)
 # Convert bibliography exported from Web of Science -> GB/T 7714—2005 format
 # Example: Kanamori H．Shaking without quaking[J]．Science，1998，279(5359)：2063-2064．
 
@@ -15,11 +16,20 @@ BP      Beginning Page
 EP      Ending Page
 '''
 
-# TBC: format AU names
+# TBC1: format AU names
 
-filepath = "savedrecs.txt"
-outputFilepath = "formatted.txt"
-targetKey = ['PT', 'AU', 'TI', 'SO', 'VL', 'IS', 'BP', 'EP', 'PY']
+# # filepath = "savedrecs.txt"
+# filepath = "bbb.txt"
+# # outputFilepath = "formatted.txt"
+# outputFilepath = "aaa2.txt"
+# act = 'reorder'
+filepath = "formatted.txt"
+act = 'wos2gb'
+# act = 'deduplicate'
+filepath = "aaa.txt"
+
+
+targetKeys = ['PT', 'AU', 'TI', 'SO', 'VL', 'IS', 'BP', 'EP', 'PY']
 pb = {}
 key = ''
 
@@ -27,19 +37,26 @@ def formatPb(pb):
     ''' Format one dictionary -> GB/T 7714—2005 bibliography
     Example: Kanamori H．Shaking without quaking[J]．Science，1998，279(5359)：2063-2064．
     '''
-    return f"{pb['AU']}. {pb['TI']}[{pb['PT']}]. {pb['SO']}, {pb['PY']}, {pb['VL']}({pb['IS']}): {pb['BP']}-{pb['EP']}.\n"
+    # BUG: reorganize code. test missing -> behavior under different missing
+    if 'IS' not in pb:
+        return f"{pb['AU']}. {pb['TI']}[{pb['PT']}]. {pb['SO']}, {pb['PY']}, {pb['VL']}: {pb['BP']}-{pb['EP']}.\n"
+    else:
+        for k in targetKeys:
+            if not k in pb:
+                print(f"'{pb['TI']}' value missing: {k}")
+                pb[k] = ''
+                # return "\n"
+        return f"{pb['AU']}. {pb['TI']}[{pb['PT']}]. {pb['SO']}, {pb['PY']}, {pb['VL']}({pb['IS']}): {pb['BP']}-{pb['EP']}.\n"
   
 def formatValue(key, raw):
-    '''
-    '''
     if key == 'AU':
         return formatAU(raw)
+    if key == 'TI':
+        return formatTI(raw)
     else: 
         return raw
 
 def appendValue(pb, key, valueField):
-    '''
-    '''
     if key == 'AU':
         pb[key] += ', ' + formatValue('AU', valueField)
     else:
@@ -47,42 +64,121 @@ def appendValue(pb, key, valueField):
 
 def formatAU(au):
     ''' Format authors -> GB/T 7714—2005
-    Example: J. C. Smith -> Smith J C; Albert Einstein -> Einstein A; Hiroo Kanamori -> Kanamori H; Liang Fujun
+    Example: J. C. Smith -> Smith J C; Albert(名) Einstein(姓) -> Einstein A; Hiroo Kanamori -> Kanamori H; Liang Fujun
     '''
-    # au.replace('.', '') #%%%
+    # TODO: s(TBC1) 
+    # 1. ISSUE: raw names are in different format "Antzaka, A.","Tao, Haicheng", "Ghosh, Kushal Kanti", "Fendt, Matthew William"
+    # 2. Chinese name detection.
+    #    What about Korean names? 
+    au = au.replace('.','')
+    [firstName, lastName] = au.split(', ') # really?%%%
+    # print(f"## {firstName}")
+    firstNames = firstName.split(' ')
+    firstNameAb = [name[0] for name in firstNames]
+    # print(f"### {firstNameAb}")
+    firstNameAb = ' '.join(firstNameAb)
+    au = ' '.join([lastName, firstNameAb]) 
     # print(au)
-    # [lastNames, firstNames] = au.split(', ')
-    # firstNames = firstNames.split( )
-    # firstName = str(firstNames[-1])
-    # print(firstName)
-    # upper = []
-    # # upper = re.#%%%
-    # # upper += re.#%%%
-    # return f"#{firstName} {' '.join(upper)}#"
-
-    [lastNames, firstNames] = au.split(', ')
-    au = ' '.join([firstNames, lastNames])
     return au
 
-with open(outputFilepath,'a') as fOut:
+def formatTI(raw):
+    # TODO: Capitalize(Aaa Bbb, not AAA BBB)
+    return raw
+
+def clearFileContent(filepath):
+    with open(filepath,'w') as fOut:
+        fOut.write('')
+
+def refreshOrder(filepath):
+    lines = []
+    with open(filepath) as f:
+        i = 1
+        for line in f:
+            index = line.find(']')
+            line = f"[{i}{line[index:]}"
+            print(line)
+            lines.append(line)
+            i += 1
+    clearFileContent(filepath) #?BUG: 没清空成功
+    with open(filepath,'a') as fOut:
+        fOut.writelines(lines)
+
+def addOrder(filepath):
+    lines = []
+    with open(filepath) as f:
+        i = 1
+        for line in f:
+            if line != '\n':
+                line = f"[{i}]{line}"
+                lines.append(line)
+                i += 1
+    clearFileContent(filepath) #?BUG: 没清空成功
+    print(lines)
+    with open(filepath,'a') as fOut:
+        fOut.writelines(lines)
+
+def deduplicate(filepath):
+    lines = []
     with open(filepath) as f:
         for line in f:
-            if line == '\n':
-                ref = formatPb(pb)
-                fOut.write(ref)
-                continue
+            index = line.find(']')
+            line = line[index+1:]
+            if line not in lines:
+                lines.append(line)
+    clearFileContent(filepath) #?BUG: 没清空成功
+    with open(filepath,'a') as fOut:
+        fOut.writelines(lines)
 
-            keyField = line[0:2]; valueField = line[3:-1]
-            if keyField == '  ': # continue last line
-                if key in targetKey:
-                    appendValue(pb, key, valueField)
-            else: 
-                key = keyField
-                if key in targetKey:
-                    pb[key] = formatValue(key, valueField)
+    clearFileContent(filepath) #?BUG: 没清空成功
+    with open(filepath,'a') as fOut:
+        fOut.writelines(lines)
 
-# outputpath = '/Users/admin/Downloads/output.txt'
-# with open(outputpath, 'w') as f:
-#     f.writelines(content)
+if act == 'wos2gb':
+    filepath = "savedrecs.txt"
+    outputFilepath = "formatted.txt"
+    with open(outputFilepath,'a') as fOut:
+        with open(filepath) as f:
+            for line in f:
+                if line == '\n':
+                    ref = formatPb(pb)
+                    fOut.write(ref)
+                    continue
+
+                keyField = line[0:2]
+                valueField = line[3:-1]
+                if keyField == '  ': # continue last key
+                    if key in targetKeys:
+                        appendValue(pb, key, valueField)
+                else: 
+                    key = keyField
+                    if key in targetKeys:
+                        pb[key] = formatValue(key, valueField)
+    addOrder(outputFilepath)
+
+if act == 'reorder':
+    refreshOrder(filepath)
+
+if act == 'deduplicate':
+    deduplicate(filepath)
+    addOrder(filepath)
+
+# with open(outputFilepath,'a') as fOut:
+#     with open(filepath) as f:
+#         for line in f:
+#             i = line.find('DOI')
+#             line = line[:i] + '\n'
+#             fOut.write(line)
 
 
+            # fOut.write(line)
+            #     continue
+
+            # keyField = line[0:2]
+            # valueField = line[3:-1]
+            # if keyField == '  ': # continue last key
+            #     if key in targetKeys:
+            #         appendValue(pb, key, valueField)
+            # else: 
+            #     key = keyField
+            #     if key in targetKeys:
+            #         pb[key] = formatValue(key, valueField)
